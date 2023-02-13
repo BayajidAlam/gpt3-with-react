b0389d58b68765ecc11d3fe56a744a45ae3c64e2
bayajidalam <bayajidalam2001@gmail.com>
2023-02-13 13:16:34 +0600

    file and folder structure ready

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article } from '../components/article/Article';
+export {default as Brand } from '../components/brand/Brand.jsx';
+export {default as CTA } from '../components/cta/CTA.jsx';
+export { default as Feature } from '../components/feature/Feature.jsx';
+export {default as Navbar } from '../components/navbar/Navbar.jsx';
