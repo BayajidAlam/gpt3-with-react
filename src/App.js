7d1161c2735a0aff257898179ba22265b4239aa7
bayajidalam <bayajidalam2001@gmail.com>
2023-02-13 14:04:50 +0600

    common css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import Brand from './components/brand/Brand'
+import CTA from './components/cta/CTA'
+import Feature from './components/feature/Feature'
+import Navbar from './components/navbar/Navbar'
+import Blog from './containers/blog/Blog'
+import Footer from './containers/footer/Footer'
+import Header from './containers/header/Header'
+import Possibility from './containers/possibility/Possibility'
+import WhatGPT3 from './containers/WhatGPT3/WhatGPT3'
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-
+    <div className='App'>
+        <div className="gradient__bg">
+            <Navbar/>
+            <Header/>
+        </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Feature/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
     </div>
   )
 }
