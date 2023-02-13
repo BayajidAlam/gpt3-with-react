99055bc3286b96202ea28768ebc1eda6668996f3
bayajidalam <bayajidalam2001@gmail.com>
2023-02-13 23:20:41 +0600

    brand section added

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './Brand.css';
+import google from '../../assets/google.png';
+import slack from '../../assets/shopify.png';
+import atlasssian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className="gpt3__brand section__padding">
+        <div>
+            <img src={google} alt="google" />
+        </div>
+        <div>
+            <img src={slack} alt="slack" />
+        </div>
+        <div>
+            <img src={atlasssian} alt="atlasssian" />
+        </div>
+        <div>
+            <img src={dropbox} alt="dropbox" />
+        </div>
+        <div>
+            <img src={shopify} alt="shopify" />
+        </div>
+    </div>
   )
 }
 
