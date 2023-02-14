025ef2954adc7e3ff371b4bc3496da9c64c25a96
bayajidalam <bayajidalam2001@gmail.com>
2023-02-15 00:28:51 +0600

    working with possibility

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Possibility.css';
+import possibility from '../../assets/possibility.png';
 
 const Possibility = () => {
   return (
-    <div id='#possibility' className='gpt3_possibility section_padding'>
-      
+    <div id='#possibility' className='gpt3_possibility section__padding'>
+      <div className="gpt3__possibility-image">
+          <img src={possibility} alt="" />
+      </div>
+
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+        The possibilities are beyond your imagination
+        </h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   )
 }
